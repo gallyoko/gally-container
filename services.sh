d761f56ecc920eb669291401524a7f929873c1b0
gally <frederic.penot@gmail.com>
2018-03-23 22:04:57 +0100

    Add files via upload

---
+++ services.sh
@@ -0,0 +1,22 @@
+login_ssh=docker
+password_ssh=docker
+
+service ssh start
+
+echo ""
+echo ""
+echo ""
+echo "###############################################################################"
+echo "##                                                                             "
+echo "##                   Bienvenue sur le container Gally                          "
+echo "##                                                                             "
+echo "##                                                                             "
+echo "##    * Connexion SSH :                                                        "
+echo "##      - host => 172.17.0.2                                                   "
+echo "##      - login => ${login_ssh}                                                "
+echo "##      - password => ${password_ssh}                                          "
+echo "##                                                                             "
+echo "###############################################################################"
+echo ""
+echo ""
+echo ""
